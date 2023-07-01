a01be973e5be253447274366e4a40e5dc9ea342b
Paulo Daniel <pdanielcdo11@gmail.com>
2023-07-01 15:07:39 -0300

    feat: add search code

---
+++ include/utils/Search.cpp
@@ -0,0 +1,38 @@
+#include "Search.h"
+
+/* Binary search has a time complexity of O(log n) */
+template <typename T>
+int binarySearch(T array[], T key, int start, int end){
+  if (start > end) {
+    // Element not found
+    return -1;
+  }
+  
+  int mid = start + (end - start) / 2;
+  
+  if (array[mid] == key) {
+    // Element found at mid index
+    return mid;
+  } else if (array[mid] > key) {
+    // Search in the left half of the array
+    return binarySearch(array, key, start, mid - 1);
+  } else {
+    // Search in the right half of the array
+    return binarySearch(array, key, mid + 1, end);
+  }
+};
+
+
+/* The sequential search has a time complexity of O(n) */
+template <typename T>
+int sequentialSearch(T array[], T key, int length){
+  for (int i = 0; i < length; i++) {
+    if (array[i] == key) {
+      // Element found at index i
+      return i;
+    }
+  }
+    
+  // Element not found
+  return -1;
+}
