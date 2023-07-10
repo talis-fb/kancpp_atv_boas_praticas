9d0914db0548d1c9fd7a3c2f9419af255f7adcb6
Pedro Paulo <pedropaulolucas@hotmail.com>
2023-07-09 21:36:53 -0300

    feat: add file manager

--- src/main.cpp
+++ src/main.cpp
@@ -6,51 +6,34 @@
 int main()
 {
   Column *column = new Column("1", "name");
-  FeatureTask *featureTask = new FeatureTask(*column, "title", "description", "project");
 
-  std::cout << featureTask->getProject() << std::endl;
+  FeatureTask featureTask1(*column, "project1");
+  FeatureTask featureTask2(*column, "project2");
 
-  FileManager *file = new FileManager("task.bin");
+  FileManager file(Filename::TASK);
 
-  if (file->isOpen())
+  if (file.isReady())
   {
-    file->write(featureTask);
-    delete file;
-    cout << "Objeto da classe FeatureTask foi persistido com sucesso no arquivo feature_tasks.bin." << endl;
+    file.write(featureTask1, featureTask2);
+    cout << "Objetos da classe FeatureTask foram persistidos com sucesso no arquivo tasks.bin." << endl;
   }
   else
   {
     cout << "Erro ao abrir o arquivo." << endl;
   }
 
-  // Ler o objeto da classe FeatureTask do arquivo
+  file.~FileManager();
 
-  FileManager *file2 = new FileManager("task.bin");
+  FileManager readFile(Filename::TASK);
 
-  if (file2->isOpen())
+  vector<FeatureTask> tasks = readFile.read<FeatureTask>();
+  cout << "Objetos lidos do arquivo:" << endl;
+  for (const FeatureTask &task : tasks)
   {
-    cout << "Arquivo aberto com sucesso." << endl;
-    FeatureTask *featureTask2 = file2->read<FeatureTask>();
-    featureTask2->print();
-    delete file2;
-
-    // if (!file2->isEOF())
-    // {
-    //   // O objeto da classe FeatureTask foi lido com sucesso do arquivo
-    //   // Agora você pode trabalhar com o objeto recuperado
-
-    //   // Lembre-se de liberar a memória alocada para o objeto featureTask
-    //   delete featureTask2;
-    // }
-    // else
-    // {
-    //   cout << "Erro ao ler o objeto da classe FeatureTask do arquivo->" << endl;
-    // }
+    task.print();
   }
-  else
-  {
-    cout << "Erro ao abrir o arquivo." << endl;
-  }
+
+  readFile.~FileManager();
 
   return 0;
 }
